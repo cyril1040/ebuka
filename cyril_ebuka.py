01cf09257630624c569edf61e002e9218f993e27
cyril1040 <66218725+cyril1040@users.noreply.github.com>
2020-06-05 00:52:07 +0100

    Add files via upload

---
+++ cyril_ebuka.py
@@ -0,0 +1,8 @@
+import json
+full_name = ' Chukwuma Cyril Chukwuebuka '
+hngi7_id = ' HNGi7 ID HNG-01411 '
+language = ' python '
+email = ' email cyrilebuka.chukwuma@gmail.com'
+print(json.dumps(" Hello World " + " this is " + full_name + " with " + hngi7_id + " using" + language.capitalize() + " for stage 2 task" + email))
+
+
