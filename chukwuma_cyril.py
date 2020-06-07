9fdf2e32f4c4dacbf9991e80f1165163848271d6
cyril1040 <66218725+cyril1040@users.noreply.github.com>
2020-06-07 22:48:54 +0100

    Add files via upload

---
+++ chukwuma_cyril.py
@@ -0,0 +1,27 @@
+full_name = input(" enter your full name: ")
+age = int(input(" enter your age: "))
+gender = input(" enter gender: ")
+number = input(" enter phone number: ")
+if age >= 18:
+    print(full_name + " welcome to our boutique" )
+our_products = {"bags": 80, "shirts": 100, "shoes": 40, "trousers": 80, "belts": 20, "underwears": 75, "cap": 10, "wrist_watch": 55}
+for key, values in our_products.items():
+    print(key, values)
+    total_cost = 0
+    while True:
+        a = input()
+        if a.lower() in our_products:
+            total_cost += our_products[a]
+        elif a == 'quit':
+            break
+        else:
+            print(' not available')
+else:
+    print(' you must be up to 18')
+
+
+
+
+
+
+
